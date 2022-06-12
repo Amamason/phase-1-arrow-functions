31acbb24e881cbeaef853874a6d108a8199a1dd4
Amamason <104336445+Amamason@users.noreply.github.com>
2022-06-12 11:07:03 +1000

    commit tree dd82255f942178ae056c97c822b58a719a87d926

---
+++ index.js
@@ -0,0 +1,25 @@
+/*const foo = function() {
+    return 'bar';
+} 
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5 
+
+const twoAdder = x => x + 2;
+// is the same as
+const twoAdder = (x) => x + 2; 
+
+const sum = (parameter1, parameter2) => {
+  console.log(`Adding ${parameter1}`);
+  console.log(`Adding ${parameter2}`);
+  return parameter1 + parameter2;
+}
+sum(1,2); //=> 3 
+
+*/
+
+function divide(){
+return (2000 / 100);
+}
+const square = (x, y) => x * x;
+
+const add = (a, b) => a + b;
